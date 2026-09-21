afcd5444698c9ad0ad04bc614422445721d1dcfd
Амир Лапшин <dudkafrudka@gmail.com>
2026-09-21 14:26:07 +0400

    Add terraform/versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.130"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
